98907655052bbf6fb506aa4b828d7956028ce076
Estefano Vilela <evilelazarate@gmail.com>
2024-01-23 15:15:06 -0500

    fixing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
       name: 'home',
       component: () => import('@/views/HomePage.vue')
     },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('@/views/AboutPage.vue')
-    }
   ]
 })
 
