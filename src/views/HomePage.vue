701ca258895fbda9c40d418ee63626f10214929f
Estefano Vilela <evilelazarate@gmail.com>
2024-01-23 14:54:45 -0500

    init

---
+++ src/views/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import { useCounterStore } from '@/stores/counter'
+const cs = useCounterStore();
+
+</script>
+
+<template>
+  <h3>Counter</h3>
+  <p>{{ cs.count }}</p>
+  <p>{{ cs.doubleCount }}</p>
+  <button @click="cs.increment">Increment</button>
+  <button @click="cs.decrement">Decrement</button>
+</template>
